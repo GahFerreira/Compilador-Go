06def72391996c6381f40a1de655a7c46b996f55
GahFerreira <49258183+GahFerreira@users.noreply.github.com>
2023-03-01 00:39:33 -0300

    Atualiza Gerador Lexico, que agora gera código compilável por Javacc

--- Gerador Lexico.py
+++ Gerador Lexico.py
@@ -1,6 +1,24 @@
 # Programa para gerar código em JavaCC de reconhecimento léxico da linguagem Go.
 
 nome_da_classe = "Compilador"
+prefixo_token = "TOKEN_"
+
+# Tokens normais: seus nomes e valores são iguais.
+tipos = [ "ANY", "BOOL", "BYTE", "COMPARABLE", "COMPLEX64", "COMPLEX128", "ERROR",
+          "FLOAT32", "FLOAT64", "INT", "INT8", "INT16", "INT32", "INT64", "RUNE",
+          "STRING", "UINT", "UINT8", "UINT16", "UINT32", "UINT64", "UINTPTR" ]
+
+constantes = [ "TRUE", "FALSE", "IOTA" ]
+
+funcoes = [ "APPEND", "CAP", "CLOSE", "COMPLEX", "COPY", "DELETE", "IMAG", "LEN",
+            "MAKE", "NEW", "PANIC", "PRINTLN", "PRINT", "REAL", "RECOVER" ]
+
+palavras_chave = [ "BREAK", "DEFAULT", "FUNC", "INTERFACE", "SELECT", "CASE",
+                   "DEFER", "GO", "MAP", "STRUCT", "CHAN", "ELSE", "GOTO",
+                   "PACKAGE", "SWITCH", "CONST", "FALLTHROUGH", "IF", "RANGE",
+                   "TYPE", "CONTINUE", "FOR", "IMPORT", "RETURN", "VAR" ]
+
+valor_zero = [ "NIL" ]
 
 # Tokens especiais: seus nomes e valores são distintos.
 operadores = \
@@ -199,50 +217,64 @@ operadores = \
   }
 ]
 
-# Tokens normais: seus nomes e valores são iguais.
-tipos = [ "ANY", "BOOL", "BYTE", "COMPARABLE", "COMPLEX64", "COMPLEX128", "ERROR",
-          "FLOAT32", "FLOAT64", "INT", "INT8", "INT16", "INT32", "INT64", "RUNE",
-          "STRING", "UINT", "UINT8", "UINT16", "UINT32", "UINT64", "UINTPTR" ]
-
-constantes = [ "TRUE", "FALSE", "IOTA" ]
-
-funcoes = [ "APPEND", "CAP", "CLOSE", "COMPLEX", "COPY", "DELETE", "IMAG", "LEN",
-            "MAKE", "NEW", "PANIC", "PRINTLN", "PRINT", "REAL", "RECOVER" ]
-
-palavras_chave = [ "BREAK", "DEFAULT", "FUNC", "INTERFACE", "SELECT", "CASE",
-                   "DEFER", "GO", "MAP", "STRUCT", "CHAN", "ELSE", "GOTO",
-                   "PACKAGE", "SWITCH", "CONST", "FALLTHROUGH", "IF", "RANGE",
-                   "TYPE", "CONTINUE", "FOR", "IMPORT", "RETURN", "VAR" ]
-
-valor_zero = [ "NIL" ]
-
-'''# Listando todas as listas de tokens
-listas_de_tokens = \
+# Em Go, apesar de '_42' ser um identificador, '0x_42' é válido.
+numeros = \
 [
   {
-    "nome_lista": "Operadores",
-    "referência": operadores
+    "nome": "DECIMAL_NUMBER",
+    "expressão regular": '"0" | (["1"-"9"] (("_")? ["0"-"9"])*)'
   },
-  { "nome_lista": "Tipos",
+  {
+    "nome": "BINARY_NUMBER",
+    "expressão regular": '"0" ("b" | "B") (("_")? ["0"-"1"])+'
+  },
+  {
+    "nome": "OCTAL_NUMBER",
+    "expressão regular": '"0" ("o" | "O")? (("_")? ["0"-"7"])+'
+  },
+  {
+    "nome": "HEXADECIMAL_NUMBER",
+    "expressão regular": '"0" ("x" | "X") ( ("_")? (["0"-"9"] | ["A"-"F"] | ["a"-"f"]) )+'
+  }
+]
+
+# Listando todas as listas de tokens
+listas_de_tokens_normais = \
+[
+  {
+    "nome": "Tipos",
     "referência": tipos
   },
   {
-    "nome_lista": "Constantes",
+    "nome": "Constantes",
     "referência": constantes
   },
   {
-    "nome_lista": "Funções",
+    "nome": "Funções",
     "referência": funcoes
   },
   {
-    "nome_lista": "Palavras-Chave",
+    "nome": "Palavras-Chave",
     "referência": palavras_chave
   },
   {
-    "nome_lista": "Valor Zero",
+    "nome": "Valor Zero",
     "referência": valor_zero
   }
-]'''
+]
+
+listas_de_tokens_especiais = \
+[
+  {
+    "nome": "Operadores",
+    "referência": operadores
+  },
+  {
+    "nome": "Números",
+    "referência": numeros
+  }
+]
+
 
 # Implementa a classe base.
 print("PARSER_BEGIN(" + nome_da_classe + ")",
@@ -280,52 +312,37 @@ print("TOKEN:",
       "{",
       sep = "\n")
 
-## OPERADORES
-print("  /** Operadores */")
+## Tokens Normais
+for lista in listas_de_tokens_normais[:-1]:
+  print("  /** " + lista["nome"] + " */")
 
-_ = [print("  < " + operador["nome"] + ': "' + operador["valor"] + '" >',
-           "|",
-           sep = "\n")
-     for operador in operadores]
+  for token in lista["referência"]:
+    print("  < " + prefixo_token + token + ': "' + token.lower() + '" >',
+          "|",
+          sep = "\n")
 
-## TIPOS
-print("  /** Tipos */")
+## Tokens Especiais
+for lista in listas_de_tokens_especiais:
+  print("  /** " + lista["nome"] + " */")
 
-_ = [print("  < " + tipo + ': "' + tipo.lower() + '" >',
-           "|",
-           sep = "\n")
-     for tipo in tipos]
+  for token in lista["referência"]:
+    if "valor" in token:
+      print("  < " + prefixo_token + token["nome"] + ': "' + token["valor"] + '" >',
+            "|",
+            sep = "\n")
 
-## CONSTANTES
-print("  /** Constantes */")
+    else:
+      print("  < " + prefixo_token + token["nome"] + ": " + token["expressão regular"] + " >",
+            "|",
+            sep = "\n")
 
-_ = [print("  < " + constante + ': "' + constante.lower() + '" >',
-           "|",
-           sep = "\n")
-     for constante in constantes]
 
-## FUNÇÕES
-print("  /** Funções */")
-
-_ = [print("  < " + funcao + ': "' + funcao.lower() + '" >',
-           "|",
-           sep = "\n")
-     for funcao in funcoes]
-
-## PALAVRAS-CHAVE
-print("  /** Palavras-Chave */")
-
-_ = [print("  < " + palavra_chave + ': "' + palavra_chave.lower() + '" >',
-           "|",
-           sep = "\n")
-     for palavra_chave in palavras_chave]
-
-## VALOR ZERO
+## Última Lista
 print("  /** Valor Zero */")
-
-print("  < " + valor_zero[0] + ': "' + valor_zero[0] + '" >', sep = "\n")
+print("  < " + prefixo_token + valor_zero[0] + ': "' + valor_zero[0] + '" >', sep = "\n")
 
 print("}\n\n")
+
 
 # Implementação da função de início.
 print("void Inicio():",
@@ -334,4 +351,38 @@ print("void Inicio():",
       "}",
       "{",
       "  (",
+      sep = "\n")
+
+for lista in listas_de_tokens_normais[:-1]:
+  for token in lista["referência"]:
+    print("    t = <" + prefixo_token + token + ">",
+          "    {",
+          '      System.out.println("' + prefixo_token + token + ' " + t.image);',
+          "    }",
+          "",
+          "    |",
+          "",
+          sep = "\n")
+
+for lista in listas_de_tokens_especiais:
+  for token in lista["referência"]:
+    print("    t = <" + prefixo_token + token["nome"] + ">",
+          "    {",
+          '      System.out.println("' + prefixo_token + token["nome"] + ' " + t.image);',
+          "    }",
+          "",
+          "    |",
+          "",
+          sep = "\n")
+
+print("    t = <" + prefixo_token + valor_zero[0] + ">",
+      "    {",
+      '      System.out.println("' + prefixo_token + valor_zero[0] + ' " + t.image);',
+      "    }",
+      sep = "\n")
+
+print("  )*",
+      "",
+      "  <EOF>",
+      "}",
       sep = "\n")
